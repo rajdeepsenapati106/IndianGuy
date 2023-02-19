849eabe09ed5d2cdb857c47e33ee1394bbfa7701
Rajdeep Senapati <raj.senapati106@gmail.com>
2023-02-19 21:09:51 +0530

    Africian Guy commit

--- hello_indian_guy/First_conversation.py
+++ hello_indian_guy/First_conversation.py
@@ -11,3 +11,9 @@ print("Hlww")
 print("I am fine & what about you ??")
 print("Yes i have done my work ... ")
 print("Yes man i have done .. 90 % of my work and what about you ?")
+
+print("===============================")
+
+print("Yeh man i am fine also :)")
+print("yes i have done too")
+print("Yes man i have done .. 100 % of my work ")
